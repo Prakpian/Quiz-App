8433bc6fc3680046aa07f2bd3908ac9a4852e855
Prakpian <143757147+Prakpian@users.noreply.github.com>
2025-03-25 22:40:21 +0000

    removed hover effect from the component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button({ btnText, onClick }) {
   return (
     <button
-      className="bg-(--color-secondary) py-2 px-4 border border-5 border-(--color-main) rounded-xl text-white w-[180px]"
+      className="bg-(--color-secondary) py-2 px-4 border border-5 border-(--color-main) rounded-xl text(--color-secondary) font-medium w-[180px]"
       onClick={onClick}
     >
       {btnText}
