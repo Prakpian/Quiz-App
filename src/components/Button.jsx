1d173aa1f0b63b5edb22697b554c23f7fa9bdd6b
Prakpian <143757147+Prakpian@users.noreply.github.com>
2025-03-25 18:51:37 +0000

    added onClick function

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-function Button({ btnText }) {
+function Button({ btnText, onClick }) {
   return (
-    <button className="bg-(--color-secondary) py-2 px-4 border border-5 border-(--color-main) rounded-xl text-white w-[180px]">
+    <button
+      className="bg-(--color-secondary) py-2 px-4 border border-5 border-(--color-main) rounded-xl text-white w-[180px]"
+      onClick={onClick}
+    >
       {btnText}
     </button>
   );
